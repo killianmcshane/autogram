cad733bf8919596631a1c80e46ec0c15e22b6d6a
Killian Jochen McShane <killian.jmcshane@gmail.com>
2021-04-15 11:43:41 +0100

    Add files via upload

--- autogram/main.py
+++ autogram/main.py
@@ -1,11 +1,15 @@
 from defines import getPaths
+import argparse
 import contentUploader
 import imgurUploader
 import contentScale
 from os import listdir
 
+parser = argparse.ArgumentParser(description="Post Frequency")
+parser.add_argument("-f", help="The post frequency (in hours).", type=int)
+
 paths = getPaths()
-post_frequency = 4  # (in hours)
+post_frequency = parser.parse_args()    # (in hours)
 
 while len(listdir(paths['content_preprocessing_path'])) != 0:
     contentScale.scaleContent()
